908d5fe863e5684331e28f5470f40dbaa33641ea
Mjohn572 <mjohn572@mtroyal.ca>
2025-01-03 17:55:48 -0700

    renamed:    main/test.txt -> main/StatementText.txt 	modified:   main/src.py

--- main/src.py
+++ main/src.py
@@ -1,32 +1,42 @@
 import re
-
+"""
+TO USE, CREATE NEW TEXT FILE, DO CTRL+SHIFT+P AND TYPE IN 'EXTRACT TEXT FROM PDF', THEN SELECT BANK STATEMENT PDF FILE
+"""
 def clean_list(file_contents: list) -> list:
 
     
-    cleaned_list = [[]] # Instantiating the final list
+    cleaned_list = [] # Instantiating the final list
 
-    for i in file_contents: # Check through all contents one by one
-        if "RETAIL PURCHASE" not in file_contents[i] and "E-transfer" not in file_contents[i]:
-            continue
-        else:
+    for i in range(len(file_contents)): # Check through each index one by one
+
+        if "RETAIL PURCHASE" in file_contents[i] or "E-transfer" in file_contents[i] or "PREAUTHORIZED DEBIT" in file_contents[i]:
+        
             # Regex to match a date at the beginning of the string (e.g., 'Nov 1', 'Dec 25', etc.)
             # and remove everything after it
             result = re.search(r'^[A-Za-z]{3} \d{1,2}', file_contents[i])
 
             # Extract the date part and store it
             if result: # The First one should always return with a date
-                cleaned_list[i].append(result.group())
+                date = result.group()
 
             else:
-                cleaned_list[i].append(cleaned_list[-1][0]) # If no match, take date from previous append
-            
-            cleaned_list[i].append(file_contents[i+1]) # Add the next line, which is the retail place
+                date = cleaned_list[-1][0] if cleaned_list else "Unknown" # If no match, take date from previous append, if no previous date make Unknown
             
 
-            if "CAD" or "USD" in file_contents[i+2]: # Checking if this line is a throwaway
-                file_contents.remove(i)
+            transaction_place = file_contents[i+1] # Add the next line, which is the retail place
 
-            cleaned_list.append(file_contents[i+2]) # Add the Amount in the line
+
+            if "CAD" in file_contents[i+2] or "USD" in file_contents[i+2] or "CAPPED MONTHLY FEE$16.95" in file_contents[i+2] or "eBay Commerce Ca" in file_contents[i+2]: # Checking if this line is a throwaway
+                # If line was throwaway get next line
+                purchase_amount = file_contents[i+3]
+            else:  
+                # If no throwaway line get index 2
+                purchase_amount = file_contents[i+2]
+            
+            # Truncate the purchased amount to only get the first number without rounding
+            purchase_amount_truncated = str(int(float(purchase_amount.split(',')[0]) * 100) / 100)
+
+            cleaned_list.append([date, transaction_place, purchase_amount_truncated])
 
     return cleaned_list
 
@@ -50,9 +60,15 @@ def main() -> None:
     Here is where everything starts and ends. 
     All functions will be called in this function
     """
-    file_name = "main/test.txt"
+    
+    file_name = "main/StatementText.txt"
+
+    #Takes the contents of the file and adds it to a list where every index is a line
     file_contents = read_file(file_name)
-    print(file_contents)
+
+    #print(file_contents) # To TEST
+
+    print(clean_list(file_contents))
 
 
     #Create List
